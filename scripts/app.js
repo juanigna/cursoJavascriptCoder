cb1b2d9847f517991a551ef8dedd4669c6348c7f
juanigna <jibocchi14@gmail.com>
2022-07-20 13:18:54 -0300

    Estructura inicial del proyecto

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
-const suma = (a,b) => a+b;
-const resta = (a,b) => a-b;
+const suma = (a, b) => a + b;
+const resta = (a, b) => a - b;
 
 let elPrincipito = 1200;
 let perroSiberiano = 2200;
@@ -7,28 +7,28 @@ let laOdisea = 3500;
 let precioFinal = 0;
 const iva = x => x * 0.21;
 
-function escogerLibro(libro){
+function escogerLibro(libro) {
   alert("Bienvenido a la tienda de libros!!!");
   let salida = "";
-  while(salida != "ESC"){
-    libro = parseInt(prompt("Escogue un libro escribiendo el numero de referencia: \n 1) El principito \n 2) El perro siberiano \n 3) La odisea "));
-    switch(libro){
-    case 1:
-      precioFinal += suma(elPrincipito, iva(elPrincipito));
-      break;
-    case 2:
-      precioFinal += suma(perroSiberiano, iva(perroSiberiano));
-      break;
-    case 3:
-      precioFinal += suma(laOdisea, iva(laOdisea));
-      break;
-    default:
-      alert("Porfavor ingrese un numero que corresponda al libro a comprar, muchas gracias!!!");
-  }
+  while (salida != "ESC") {
+    libro = parseInt(prompt("Escoge un libro escribiendo el numero de referencia: \n 1) El principito \n 2) El perro siberiano \n 3) La odisea "));
+    switch (libro) {
+      case 1:
+        precioFinal += suma(elPrincipito, iva(elPrincipito));
+        break;
+      case 2:
+        precioFinal += suma(perroSiberiano, iva(perroSiberiano));
+        break;
+      case 3:
+        precioFinal += suma(laOdisea, iva(laOdisea));
+        break;
+      default:
+        alert("Porfavor ingrese un numero que corresponda al libro a comprar, muchas gracias!!!");
+    }
     salida = prompt("Si quieres seguir comprando solo da enter, de lo contrario escrbibe ESC");
-   
-}
-   alert("El total a pagar + IVA es: $"+ precioFinal);
+
   }
-  
+  alert("El total a pagar + IVA es: $" + precioFinal);
+}
+
 escogerLibro();
