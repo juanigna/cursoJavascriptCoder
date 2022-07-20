5339d250155b54269a654af1e1ec38d8b7268132
juanigna <jibocchi14@gmail.com>
2022-07-20 16:02:19 -0300

    Agrego el logo a la pagina

--- scripts/app.js
+++ scripts/app.js
@@ -16,9 +16,11 @@ const iva = x => x * 0.21;
 //FUNCION QUE LEE EL LIBRO Y TE DEVUELVE EL PRECIO + IVA
 function escogerLibro(libro) {
   alert("Bienvenido a la tienda de libros!!!");
+  //VARIABLE SALIDA PARA PODER SALIR DEL CICLO
   let salida = "";
   while (salida != "ESC") {
     libro = parseInt(prompt("Escoge un libro escribiendo el numero de referencia: \n 1) El principito $" +elPrincipito+"  \n 2) El perro siberiano $"+perroSiberiano+" \n 3) La odisea $"+laOdisea));
+    //SE ME OCURRIO USAR EL SWITCH YA QUE SOLO USO 3 LIBROS SOLAMENTE
     switch (libro) {
       case 1:
         precioFinal += suma(elPrincipito, iva(elPrincipito));
