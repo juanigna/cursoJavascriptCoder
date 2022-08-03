e69a6172ef460862dd08a6174509ee3613945c4d
juanigna <jibocchi14@gmail.com>
2022-08-03 00:45:38 -0300

    Nueva seccion de futuros libros

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,4 @@
 
-let nombreUsuario = prompt("Hola! Cual es su nombre? ");
-
-function nombreBienvenida(nombre){
-  let headerSection = document.getElementById("header-portada");
-  let titulo = document.createElement("h1");
-  titulo.className= "bold fs-1 text-white text-center mt-4";
-  let mensaje = `Bienvenido! ${nombre}`;
-  titulo.innerHTML = mensaje;
-  headerSection.appendChild(titulo);
-}
-
-
-// while(nombreUsuario===null){
-//   alert("Nombre no valido.")
-//   nombreUsuario = prompt("Hola! Cual es su nombre? ");
-// }
-nombreBienvenida(nombreUsuario);
 
 class Libro{
   constructor(nombre, precio, autor, anio, img){
@@ -56,7 +39,7 @@ libros.forEach(libro =>libro.precioFinal());
 
 //FUNCION PARA ENCOTRAR EL NOMBRE DEL LIBRO
 function findName(array, nombre){
-  return array.find(title => title.nombre == nombre);
+  return array.find(title => title.nombre.toUpperCase() == nombre);
 }
 
 
@@ -67,14 +50,17 @@ function ordenarNums(array){
 }
 
 
+//ORDENO EL ARRAY PARA QUE LOS LIBROS ESTEN ORDENADOS POR EL PRECIO DE MANERA ASCENDENTE
+libros.sort((a,b)=> a.precio - b.precio);
+
 let cards = document.getElementById("card-container");
 
 for(const libro of libros){
   let card = document.createElement("div");
-  card.className = "card p-0";
+  card.className = "card p-0 justify-content-center align-items-center text-center";
   card.innerHTML = `
     <img src="${libro.img}" class="card-img-top" alt="...">
-    <div class="card-body">
+    <div class="card-body justify-content-center">
       <h5 class="card-title">${libro.nombre}</h5>
       <p class="card-text">Autor: ${libro.autor}</p>
       <p class="card-text">Precio: $${libro.precio}</p>
@@ -86,61 +72,94 @@ for(const libro of libros){
 
 
 
+let nombreUsuario = prompt("Hola! Cual es su nombre? ");
 
-// //FUNCION PARA MOSTRAR LOS DATOS DE LOS LIBROS AGREGADOS POR EL USUARIO
-// function imprimirDatos(cadena, array){
-//   let mostrar = array.map(libro=>"Nombre: " + libro.nombre +"\n" +"Año: "+ libro.anio +"\n");
-//   alert(cadena+ "\n"+mostrar);
+function nombreBienvenida(nombre){
+  let headerSection = document.getElementById("header-portada");
+  let titulo = document.createElement("h1");
+  titulo.className= "bold fs-1 text-white text-center mt-4";
+  let mensaje = `Bienvenido! ${nombre}`;
+  titulo.innerHTML = mensaje;
+  headerSection.appendChild(titulo);
+}
+
+nombreBienvenida(nombreUsuario);
+
+//FUNCION PARA MOSTRAR LOS DATOS DE LOS LIBROS AGREGADOS POR EL USUARIO
+function imprimirDatos(cadena, array){
+  let mostrar = array.map(libro=>"Nombre: " + libro.nombre +"\n" +"Año: "+ libro.anio +"\n");
+  alert(cadena+ "\n"+mostrar);
  
-// }
-// function escogerLibro(libro) {
-//   alert("Bienvenido a la tienda de libros!!!");
-//   //VARIABLE SALIDA PARA PODER SALIR DEL CICLO
-//   let salida = "";
-//   let precioFinal = 0;
-//   let titulo;
+}
+function escogerLibro(libro) {
+  alert("Bienvenido a la tienda de libros!!!");
+  //VARIABLE SALIDA PARA PODER SALIR DEL CICLO
+  let salida = "";
+  let precioFinal = 0;
+  let titulo;
   
-//   while (salida != "ESC") {
-//     titulo = prompt("Escoge un libro escribiendo el nombre tal cual se lee: \n 1) El principito $" +libros[0].precio+"  \n 2) El perro siberiano $"+libros[1].precio+" \n 3) La odisea $"+libros[2].precio).toLowerCase();
-//     let opcion = findName(libros, titulo);
+  while (salida != "ESC") {
+    titulo = prompt("Escoge un libro escribiendo el nombre tal cual se lee: \n 1) El principito $" +libros[0].precio+"  \n 2) El perro siberiano $"+libros[1].precio+" \n 3) La odisea $"+libros[2].precio).toUpperCase();
+    let opcion = findName(libros, titulo);
 
-//     //CONDICIONAL PARA VERIFICAR SI EL NOMBRE DEL LIBRO INGRESADO CONCUERDA CON UNO GUARDADO
-//     if(opcion != undefined){
-//       precioFinal += opcion.precio;
-//     }else{
-//       alert("Ingreso un nombre incorrecto");
-//     }
-//     salida = prompt("Si quieres seguir comprando solo da enter, de lo contrario escrbibe ESC o esc").toUpperCase();
-//   }
-//   alert("El total a pagar + IVA es: $" + precioFinal);
-// }
+    //CONDICIONAL PARA VERIFICAR SI EL NOMBRE DEL LIBRO INGRESADO CONCUERDA CON UNO GUARDADO
+    if(opcion != undefined){
+      precioFinal += opcion.precio;
+    }else{
+      alert("Ingreso un nombre incorrecto");
+    }
+    salida = prompt("Si quieres seguir comprando solo da enter, de lo contrario escrbibe ESC o esc").toUpperCase();
+  }
+  alert("El total a pagar + IVA es: $" + precioFinal);
+}
 
-// //CREO UNA FUNCION QUE SE ENCARGA DE AÑADIR UN LIBRO QUE SOLICITA EL USUARIO
-// function futuroLibro(){
-//   let salida = prompt("Si desea agregar un libro para que lo tengamos en cuenta solo apreta enter de lo contrario escribe ESC o esc").toUpperCase();
-//   while(salida != "ESC"){
-//     let nombre = prompt("Escriba el nombre del libro");
-//     let autor = prompt("Escriba el autor");
-//     let año = parseInt(prompt("Escriba el año"));
-//     //AÑADO AL ARRAY LOS FUTUROS LIBROS A TENER EN CUENTA SIN TENIENDO EN CUENTA EL PRECIO;
-//     futurosLibros.push(new Libro(nombre, 0, autor, año));
+//CREO UNA FUNCION QUE SE ENCARGA DE AÑADIR UN LIBRO QUE SOLICITA EL USUARIO
+function futuroLibro(){
+  let salida = prompt("Si desea agregar un libro para que lo tengamos en cuenta solo apreta enter de lo contrario escribe ESC o esc").toUpperCase();
+  while(salida != "ESC"){
+    let nombre = prompt("Escriba el nombre del libro");
+    let autor = prompt("Escriba el autor");
+    let año = parseInt(prompt("Escriba el año"));
+    //AÑADO AL ARRAY LOS FUTUROS LIBROS A TENER EN CUENTA SIN TENIENDO EN CUENTA EL PRECIO;
+    futurosLibros.push(new Libro(nombre,0,autor, año));
 
-//     //ORDENO LOS LIBROS SEGUN SU AÑO DE FORMA ASCENDENTE
-//     ordenarNums(futurosLibros);
-//     salida = prompt("Si desea agregar un libro para que lo tengamos en cuenta solo apreta enter de lo contrario escribe ESC o esc").toUpperCase();
+    //ORDENO LOS LIBROS SEGUN SU AÑO DE FORMA ASCENDENTE
+    ordenarNums(futurosLibros);
+    salida = prompt("Si desea agregar un libro para que lo tengamos en cuenta solo apreta enter de lo contrario escribe ESC o esc").toUpperCase();
     
-//   }
-//   if(futurosLibros.length > 0){
-//  //IMPRIMO EN PANTALLA LOS LIBROS AGREGADOS POR EL USUARIOa
-//   imprimirDatos("Los libros que usted agrego para que tengamos en cuenta son: ",futurosLibros);
-//   }else{
-//     alert("Muchas gracias!!!");
-//   }
+  }
+    alert("Muchas gracias!!!");
+  
  
-// }
+}
 
-// // INVOCACION DE LA FUNCION escogerLibro
-// escogerLibro();
+// INVOCACION DE LA FUNCION escogerLibro
+escogerLibro();
 
-// //INVOCO LA FUNCION fututroLibro
-// futuroLibro();
+//INVOCO LA FUNCION fututroLibro
+futuroLibro();
+
+
+//creo la varibale para utilizar la seccion de los futuros libros
+let futurosSeccion = document.getElementById("futuros-libros");
+//creo la tabla desde js y su cuerpo
+let tabla = document.createElement("table");
+tabla.className ="table table-striped";
+let tbody = document.createElement("tbody");
+
+//recorro el array de los futuros libros
+for(const libro of futurosLibros){
+  //agrego al body de la tabla los datos de los libros
+  tbody.innerHTML += `
+      <tr>
+          <th>${libro.nombre}<th>
+          <th>${libro.autor}<th>
+          <th>${libro.anio}<th>
+      </tr>
+  `
+}
+//añado a la tabla el cuerpo
+tabla.appendChild(tbody);
+
+//finalmente añado la tabla a la seccion
+futurosSeccion.appendChild(tabla);
